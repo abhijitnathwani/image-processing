24702f56e51c0309a764522a0bea6aacb54659a3
waleed <eng.0waleed@gmail.com>
2022-11-29 11:18:41 -0800

    test

--- image_rotate.c
+++ image_rotate.c
@@ -7,46 +7,33 @@
 */
 #include <stdio.h>
 
-int main()
+int image_rotate(unsigned char header[54], int height, int width, unsigned char buffer[width][height][3], unsigned char colorTable[1024])
 {
-	FILE *fIn = fopen("lena512.bmp","r");					//Input File name
-	FILE *fOut = fopen("lena_rotate.bmp","w+");		    	//Output File name
-
+	FILE *fOut = fopen("rgb_rotate.bmp","w+");		    	//Output File name
 	int i,j,choice;
-	unsigned char byte[54],colorTable[1024];
-	
-	if(fIn==NULL)											// check if the input file has not been opened succesfully.
-	{											
-		printf("File does not exist.\n");
-	}
+	int temp;
+	unsigned char heightA[4];
+	unsigned char widthA[4];
 
-	for(i=0;i<54;i++)										//read the 54 byte header from fIn
-	{									
-		byte[i] = getc(fIn);								
-	}
 
-	fwrite(byte,sizeof(unsigned char),54,fOut);				//write the header back
-
-	// extract image height, width and bitDepth from imageHeader 
-	int height = *(int*)&byte[18];
-	int width = *(int*)&byte[22];
-	int bitDepth = *(int*)&byte[28];
+	// extract image height, width and bitDepth from imageHeader
+	// int height = *(int *)&header[18];
+	// int width = *(int *)&header[22];
+	int bitDepth = *(int *)&header[28];
 
 	printf("width: %d\n",width);
 	printf("height: %d\n",height );
 
 	int size = height*width;								//calculate image size
 
-	if(bitDepth <= 8)										//if ColorTable present, extract it.
-	{
-		fread(colorTable,sizeof(unsigned char),1024,fIn);
-		fwrite(colorTable,sizeof(unsigned char),1024,fOut);
-	}
 
-	unsigned char buffer[width][height];					//to store the image data
-	unsigned char out_buffer[width][height];
 
-	fread(buffer,sizeof(unsigned char),size,fIn);			//read the image data
+	// fread(buffer,sizeof(unsigned char),size,fIn);			//read the image data
+
+	unsigned char out_buffer[width][height][3];
+	// unsigned char F_out_buffer[height][width];
+
+
 
 	printf("Enter your choice :\n");
 	printf("1. Rotate right\n");
@@ -58,39 +45,67 @@ int main()
 	switch(choice)												//to rotate image in 3 direction
 	{												
 		case 1:
+			temp=height;
+			height=width;
+			width=temp;
 			for(i=0;i<width;i++)								//to rotate right
 			{
 				for(j=0;j<height;j++)
 				{
-					out_buffer[j][height-1-i]=buffer[i][j];
+					out_buffer[j][height-1-i][2]=buffer[j][i][2];
+                	out_buffer[j][height-1-i][1]=buffer[j][i][1];
+                	out_buffer[j][height-1-i][0]=buffer[j][i][0];
 				}
 			}
-			break;
-		case 2:
-			for(i=0;i<width;i++)								//to rotate left
+
+			for (i=0;i<4;i++){
+				heightA[i]=header[18+i];
+				widthA[i]=header[22+i];
+			}
+			for (i=0;i<4;i++){
+				header[18+i]=widthA[i];
+				header[22+i]=heightA[i];
+			}
+			fwrite(header, sizeof(unsigned char), 54, fOut); // write the header back
+			if (bitDepth <= 8)								 // if ColorTable present, extract it.
 			{
-				for(j=0;j<height;j++)
-				{
-					out_buffer[j][i]=buffer[i][j];
-				}
+				fwrite(colorTable, sizeof(unsigned char), 1024, fOut);
 			}
-			break;
-		case 3:
-			for(i=0;i<width;i++)								//to rotate 180 degree
+			for (i = 0; i < height; i++)
 			{
-				for(j=0;j<height;j++)
+				for (j = 0; j < width; j++)
 				{
-					out_buffer[width-i][j]=buffer[i][j];
+					putc(out_buffer[i][j][2], fOut);
+					putc(out_buffer[i][j][1], fOut);
+					putc(out_buffer[i][j][0], fOut);
 				}
 			}
 			break;
+		// case 2:
+		// 	for(i=0;i<width;i++)								//to rotate left
+		// 	{
+		// 		for(j=0;j<height;j++)
+		// 		{
+		// 			out_buffer[j][i]=buffer[i][j];
+		// 		}
+		// 	}
+		// 	break;
+		// case 3:
+		// 	for(i=0;i<width;i++)								//to rotate 180 degree
+		// 	{
+		// 		for(j=0;j<height;j++)
+		// 		{
+		// 			out_buffer[width-i][j]=buffer[i][j];
+		// 		}
+		// 	}
+		// 	break;
 		default:
 			break;
 	}
 	
-	fwrite(out_buffer,sizeof(unsigned char),size,fOut);		//write back to the output image
+	// fwrite(out_buffer,sizeof(unsigned char),size,fOut);		//write back to the output image
 
-	fclose(fIn);
+	// fclose(fIn);
 	fclose(fOut);
 
 	return 0;
